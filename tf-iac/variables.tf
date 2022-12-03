9151d56602da5091437d1cb41d90a7c8ecdb202f
h4ndzdatm0ld <hugotinoco@icloud.com>
2022-12-03 18:20:25 +0000

    Refactoring/Testing

--- tf-iac/variables.tf
+++ tf-iac/variables.tf
@@ -1,48 +1,48 @@
 variable "region" {
   description = "AWS region name"
   type     = string
-  default      = "us-east-1"
+  default      = "us-west-2"
 }
 
-variable "vpcid" {
-  description = "Enter VPC ID to launch the FTD Appliances"
-  type        = string
-  default     = "vpc-0208ff9f4d7847db1"
-}
+# variable "vpcid" {
+#   description = "Enter VPC ID to launch the FTD Appliances"
+#   type        = string
+#   default     = "vpc-0208ff9f4d7847db1"
+# }
 
 variable "azone" {
   description = "Availability Zone"
-  default     = "us-east-1a"
+  default     = "us-west-2a"
   type        = string
 }
 
 variable "outsubnet" {
   description = "Outside subnet for firewall (ex- 22.0.1.0/24)"
-  default     = "22.0.1.0/24"
+  default     = "10.0.100.0/24"
   type        = string
 }
 
 variable "insubnet" {
   description = "Inside subnet for firewall (ex- 22.0.2.0/24)"
-  default     = "22.0.2.0/24"
+  default     = "10.0.101.0/24"
   type        = string
 }
 
 variable "mgmtsubnet" {
   description = "Management subnet for firewall (ex- 22.0.3.0/24)"
-  default     = "22.0.3.0/24"
+  default     = "10.0.102.0/24"
   type        = string
 }
 
 variable "key_name" {
   description = "AWS keypair name"
-  default     = "KubKey"
+  default     = "aws-generic"
   type        = string
 }
 
 variable "instance_count" {
   description = "Number of firewall appliances"
-  default     = 2
+  default     = 1
   type        = number
 }
 
@@ -60,10 +60,10 @@ variable "region_ami" {
     eu-west-1      = "ami-need to update these values"
     eu-west-2      = "ami-need to update these values"
     eu-west-3      = "ami-need to update these values"
-    us-east-1      = "ami-07e4a27f672a46522"
+    us-east-1      = "ami-need to update these values"
     us-east-2      = "ami-need to update these values"
     us-west-1      = "ami-need to update these values"
-    us-west-2      = "ami-need to update these values"
+    us-west-2      = "ami-0c3bfabfd60689331"
     sa-east-1      = "ami-need to update these values"
     ap-northeast-1 = "ami-need to update these values"
     ap-northeast-2 = "ami-need to update these values"
